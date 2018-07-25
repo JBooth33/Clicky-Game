eb28a1f399e64d33d104ad7b9828a7d9a78a4316
Joe Booth <joseph.booth3@gmail.com>
2018-07-25 16:36:10 -0500

    adding CubCard component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
+import CubCard from "./components/CubCard";
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <Jumbotron />
+        <CubCard />
       </div>
     );
   }
---
+++ src/components/CubCard/CubCard.js
@@ -0,0 +1,15 @@
+//set up Navbar component
+import React, { Component } from "react";
+import "./CubCard.css";
+
+class CubCard extends Component {
+    render() {
+        return (
+            <div className="card" >
+                <img className="card-img-top" src="#" alt="Card image cap" />
+            </div>
+        )
+    }
+}
+
+export default CubCard;
